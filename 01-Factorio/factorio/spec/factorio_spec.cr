c8f194d29cc89b5765418aac139a8ed4e4479d72
Borja Garibotti <mgaribotti@fi.uba.ar>
2023-05-21 14:45:39 -0300

    Refactorio

--- 01-Factorio/factorio/spec/factorio_spec.cr
+++ 01-Factorio/factorio/spec/factorio_spec.cr
@@ -1,35 +1,66 @@
 require "./spec_helper"
 
 describe Factorio do
-  it "extractor caja" do
-    contexto = Contexto.new
-    contexto.configurar_circuito_extractor_caja
-    contexto.hacer_andar_circuito_extractor_caja
+  extractor_de_carbon = Extractor.new :carbon
+  extractor_de_hierro = Extractor.new :hierro
+  cinta_de_carbon = Cinta.new
+  cinta_de_hierro = Cinta.new
+  contenedor = Contenedor.new
 
-    contexto.contenedor.cantidad_de(:carbon).should eq 1
-    contexto.contenedor.cantidad_de_menas.should eq 1
+  after_each do
+    extractor_de_carbon = Extractor.new :carbon
+    extractor_de_hierro = Extractor.new :hierro
+    cinta_de_carbon = Cinta.new
+    cinta_de_hierro = Cinta.new
+    contenedor = Contenedor.new
+  end
+
+  it "extractor caja" do
+    configurar_circuito_extractor_caja(extractor_de_carbon, contenedor)
+    hacer_andar_circuito_extractor_caja(extractor_de_carbon, contenedor)
+
+    contenedor.cantidad_de(:carbon).should eq 1
+    contenedor.cantidad_de_menas.should eq 1
   end
 
   it "extractor cinta caja" do
-    contexto = Contexto.new
-    contexto.configurar_circuito_extractor_cinta_caja
-    contexto.hacer_andar_circuito_extractor_cinta_caja
+    configurar_circuito_extractor_cinta_caja(
+      extractor_de_carbon,
+      cinta_de_carbon,
+      contenedor
+    )
+    hacer_andar_circuito_extractor_cinta_caja(
+      extractor_de_carbon,
+      cinta_de_carbon,
+      contenedor
+    )
 
-    contexto.contenedor.cantidad_de(:carbon).should eq 1
-    contexto.contenedor.cantidad_de_menas.should eq 1
-    contexto.cinta.cantidad_de(:carbon).should eq 0
-    contexto.cinta.cantidad_de_menas.should eq 0
+    contenedor.cantidad_de(:carbon).should eq 1
+    contenedor.cantidad_de_menas.should eq 1
+    cinta_de_carbon.cantidad_de(:carbon).should eq 0
+    cinta_de_carbon.cantidad_de_menas.should eq 0
   end
 
   it "extractor extractor cinta cinta caja" do
-    contexto = Contexto.new
-    contexto.configurar_circuito_extractor_extractor_cinta_cinta_caja
-    contexto.hacer_andar_circuito_extractor_extractor_cinta_cinta_caja
+    configurar_circuito_extractor_extractor_cinta_cinta_caja(
+      extractor_de_carbon,
+      extractor_de_hierro,
+      cinta_de_carbon,
+      cinta_de_hierro,
+      contenedor
+    )
+    hacer_andar_circuito_extractor_extractor_cinta_cinta_caja(
+      extractor_de_carbon,
+      extractor_de_hierro,
+      cinta_de_carbon,
+      cinta_de_hierro,
+      contenedor
+    )
 
-    contexto.contenedor.cantidad_de(:carbon).should eq 1
-    contexto.contenedor.cantidad_de(:hierro).should eq 1
-    contexto.contenedor.cantidad_de_menas.should eq 2
-    contexto.cinta.cantidad_de(:carbon).should eq 0
-    contexto.cinta.cantidad_de_menas.should eq 0
+    contenedor.cantidad_de(:carbon).should eq 1
+    contenedor.cantidad_de(:hierro).should eq 1
+    contenedor.cantidad_de_menas.should eq 2
+    cinta_de_carbon.cantidad_de(:carbon).should eq 0
+    cinta_de_carbon.cantidad_de_menas.should eq 0
   end
 end
